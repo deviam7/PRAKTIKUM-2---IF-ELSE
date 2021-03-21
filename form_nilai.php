4bbe2c764149c514bfbf22204bd6cd521b68aa09
Devi Amalia <73010431+deviam7@users.noreply.github.com>
2021-03-21 11:38:10 +0700

    PRAKTIKUM 2

---
+++ form_nilai.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<h2 class="form-login-heading"> <span class="glyphicon glyphicon-lock"></span> Praktikum 1 </h2>
+    <div class="container mt-5">
+    <div class="form-group">
+    <input type="hidden" name="id" id="id">
+        <form method="GET" action="array_siswa.php">
+            <div class="form-group row">
+                <label for="nama" class="col-4 col-form-label">Nama</label> 
+                <div class="col-8">
+                    <input id="nama" name="nama" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nim" class="col-4 col-form-label">NIM</label> 
+                <div class="col-8">
+                    <input id="nim" name="nim" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+                <div class="col-8">
+                    <select id="matkul" name="matkul" class="custom-select">
+                        <option value="DDP">Dasar-Dasar Pemrograman</option>
+                        <option value="BDI">Basis Data I</option>
+                        <option value="WEB1">Pemrograman Web</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai_uts" class="col-4 col-form-label">Nilai UTS</label> 
+                <div class="col-8">
+                    <input id="nilai_uts" name="nilai_uts" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai_uas" class="col-4 col-form-label">Nilai UAS</label> 
+                <div class="col-8">
+                    <input id="nilai_uas" name="nilai_uas" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai_tugas" class="col-4 col-form-label">Nilai Tugas/Praktikum</label> 
+                <div class="col-8">
+                    <input id="nilai_tugas" name="nilai_tugas" type="text" class="form-control">
+                </div>
+            </div> 
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
